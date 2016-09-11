a534d21c106ad842270239a1aa76078d8dc7670f
Jerome de Tychey <jdetychey@gmail.com>
2016-09-11 12:25:19 +0200

    Create FactoryWithBlockoneID.sol

---
+++ contracts/FactoryWithBlockoneID.sol
@@ -0,0 +1,33 @@
+contract EntitlementRegistry{function get(string _name)constant returns(address );function getOrThrow(string _name)constant returns(address );}
+contract Entitlement{function isEntitled(address _address)constant returns(bool );}
+
+contract MyContract {
+
+  // BlockOne ID bindings
+
+  // The address below is for the Edgware network only
+  EntitlementRegistry entitlementRegistry = EntitlementRegistry(0xe5483c010d0f50ac93a341ef5428244c84043b54);
+
+  function getEntitlement() constant returns(address) {
+      return entitlementRegistry.getOrThrow("e.g.com.tr/rideeth");
+  }
+
+  modifier entitledUsersOnly {
+    if (!Entitlement(getEntitlement()).isEntitled(msg.sender)) throw;
+    _;
+  }
+
+  // RideEth Factory can only be used by entitled users!
+  
+  import "Ride.sol"; //here goes the git repo
+
+contract RideFactory {
+    address[] public rides;
+    
+    function newRide(uint _from, uint _to) entitledUsersOnly {
+        rides.push((new Ride).value(msg.value)(_from, _to, msg.sender));
+    }
+}
+
+}
+    
